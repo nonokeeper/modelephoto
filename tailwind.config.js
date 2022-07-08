23f2288cc9c2d2c8efb3ef72f08c2d6731f887a4
costearnaud <costearnaud@yahoo.fr>
2022-07-08 17:54:17 +0200

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -6,7 +5,6 @@ module.exports = {
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
   ],
   theme: {
     extend: {},
